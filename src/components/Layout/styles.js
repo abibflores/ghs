625599fa6f04d7dadb9ceb84f42e86c26fb4a1de
abibflores <abib.m@reworth.co>
2022-05-24 23:33:10 -0500

    feat(Molecule): add Layout component

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StylesLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    min-height: 100vh;
+    align-items: center;
+`;
+
+export const StylesLayoutChildren = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px 0px;
+`;
