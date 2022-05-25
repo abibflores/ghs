a481e65bfc2ec3a7abe139ec8e7ef1e6cada2c6e
abibflores <abib.m@reworth.co>
2022-05-24 19:24:40 -0500

    feat(Molecule): add Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ContHeaderStyle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 0px 20px;
+    background: ${({theme}) => theme.colors.primary};
+    padding: 20px;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+`;
+
+export const StylesNav = styled.nav`
+    display: flex;
+    background: transparent;
+    gap: 0px 20px;
+`;
